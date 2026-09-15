7f19d2a9aaf7c2827ad317b383e056ce31ae420e
Sagar D <sagardg33@gmail.com>
2026-09-15 09:05:47 +0530

    Error handling for api routes and frontend pages

--- main.py
+++ main.py
@@ -1,47 +1,115 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, HTTPException, Request, status
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from starlette.exceptions import HTTPException as StarletteHTTPException
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-posts: list[dict]  = [
+posts: list[dict] = [
     {
         "id": 1,
         "author": "John Doe",
         "title": "My First Post",
         "content": "This is the content of my first post.",
-        "date_posted": "2023-01-01"
-    }, 
+        "date_posted": "2023-01-01",
+    },
     {
         "id": 2,
         "author": "Jane Smith",
         "title": "My Second Post",
         "content": "This is the content of my second post.",
-        "date_posted": "2023-01-02"
+        "date_posted": "2023-01-02",
     },
     {
         "id": 3,
         "author": "Alice Johnson",
         "title": "My Third Post",
         "content": "This is the content of my third post.",
-        "date_posted": "2023-01-03"
-    }
+        "date_posted": "2023-01-03",
+    },
 ]
 
 
+# Routes for the web application------------------------------------------------------------------
 @app.get("/", include_in_schema=False, name="home")
-def get_home(request: Request):
-    return templates.TemplateResponse(request, "home.html", {"posts": posts, "title": "Home"})
+@app.get("/posts", include_in_schema=False, name="posts")
+def get_home_page(request: Request):
+    return templates.TemplateResponse(
+        request, "home.html", {"posts": posts, "title": "Home"}
+    )
 
+
+@app.get("/post/{post_id}", include_in_schema=False, name="post_page")
+def post_page(request: Request, post_id: int):
+    for post in posts:
+        if post.get("id") == post_id:
+            return templates.TemplateResponse(
+                request, "post.html", {"post": post, "title": post.get("title")}
+            )
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
+
+
+# API routes--------------------------------------------------------------------------------------
 @app.get("/api/posts")
 def get_posts():
     return posts
 
-@app.get("api/posts/{post_id}")
+
+@app.get("/api/posts/{post_id}")
 def get_post(post_id: int):
     for post in posts:
         if post.get("id") == post_id:
             return post
-    return {"error": "post not found"}
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail=f"Post with id {post_id} not found",
+    )
+
+
+# Exception Handlers------------------------------------------------------------------------------
+@app.exception_handler(StarletteHTTPException)
+def general_exception_handler(request: Request, exception: StarletteHTTPException):
+    message = {
+        exception.detail
+        if exception.detail
+        else "An error occurred, check your request and try again."
+    }
+
+    if request.url.path.startswith("/api"):
+        return JSONResponse(
+            status_code=exception.status_code, content={"detail": message}
+        )
+
+    return templates.TemplateResponse(
+        request,
+        "error.html",
+        {
+            "status_code": exception.status_code,
+            "title": exception.status_code,
+            "message": message,
+        },
+        status_code=exception.status_code,
+    )
+
+@app.exception_handler(RequestValidationError)
+def validation_exception_handler(request: Request, exception: RequestValidationError):
+    if request.url.path.startswith("/api"):
+        return JSONResponse(
+            status_code=status.HTTP_422_UNPROCESSABLE_CONTENT, 
+            content={"detail": exception.errors()}
+        )
+
+    return templates.TemplateResponse(
+        request,
+        "error.html",
+        {
+            "status_code": status.HTTP_422_UNPROCESSABLE_CONTENT,
+            "title": status.HTTP_422_UNPROCESSABLE_CONTENT,
+            "message": "Invalid request, check your input and try again.",
+        },
+        status_code=status.HTTP_422_UNPROCESSABLE_CONTENT,
+    )
